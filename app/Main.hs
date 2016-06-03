847d6508ca2f0fcc963d4c3e36dc82553c24b554
meditans <meditans@gmail.com>
2016-06-03 18:45:39 +0200

    Added command line option parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,29 +1,38 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# OPTIONS_GHC -fdefer-typed-holes #-}
 
 module Main where
 
-import Control.Foldl (list)
-import Data.List (foldl1', intersperse)
+import Control.Foldl   (list)
+import Data.List       (foldl1', intersperse)
+import Options.Generic (ParseRecord, Generic, getRecord)
 import Turtle
 
-data Libraries = Libraries { resolver  :: Text
-                           , libraries :: [Text]
-                           } deriving (Eq, Show)
+data Options = Options { docsetName :: Text
+                       , resolver   :: Text
+                       , libraries  :: [Text]
+                       } deriving (Eq, Generic, Show)
+
+instance ParseRecord Options
+
+-- Make sure haddocset is installed. Example usage:
+-- stack exec autoDocset -- --docsetName nuovissimo --resolver lts-6.1 --libraries turtle --libraries foldl
 
 main :: IO ()
 main = do
-  let lib = Libraries "lts-6.1" ["turtle", "foldl"]
+  opt <- getRecord "autoDocset"
   libLocations <- flip fold list
-                $ grep (contains . text $ resolver lib)
-                $ grep (foldl1 (<|>) $ map (contains . text) (libraries lib))
-                $ grep (ends . text $ ".conf")
-                $ format fp <$> lstree "/home/carlo/.stack/snapshots"
+    $ grep (contains . text $ resolver opt)
+    $ grep (foldl1 (<|>) $ map (contains . text) (libraries opt))
+    $ grep (ends . text $ ".conf")
+    $ format fp <$> lstree "/home/carlo/.stack/snapshots"
   let libString = foldl1' (<>) . intersperse " " $ libLocations
-  command "stack exec -- haddocset -t nuovo --no-global-packages create"
+  command ("stack exec -- haddocset -t " <> docsetName opt <> " --no-global-packages create")
   command "stack build --haddock"
-  command ("stack exec -- haddocset -t nuovo.docset add " <> libString)
-  command "tar --exclude='.DS_Store' -cvzf nuovo.tgz nuovo.docset"
+  command ("stack exec -- haddocset -t " <> docsetName opt <> ".docset add " <> libString)
+  command ("tar --exclude='.DS_Store' -cvzf " <> docsetName opt <> ".tgz " <> docsetName opt <> ".docset")
+  command ("rm -r " <> docsetName opt <> ".docset")
 
 command :: MonadIO m => Text -> m ()
 command c = void (shell c empty)
